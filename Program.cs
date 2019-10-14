e33e9ed63f0d8aef720a058c3216beafe3967047
WoMspace <alex0002b@gmail.com>
2019-10-14 22:37:37 +0100

    Switch to ulongs
    Switched all longs to ulongs because what are negative numbers?

--- Program.cs
+++ Program.cs
@@ -13,18 +13,18 @@ namespace primefinder
         {
             primeLoop();
         }
-        static List<long> primes = new List<long> //the list of found prime numbers.
+        static List<ulong> primes = new List<ulong> //the list of found prime numbers.
         {
             3,
             5
         };
-        static bool checkNumber(long numberToCheck) //checks the number against the list of existing primes.
+        static bool checkNumber(ulong numberToCheck) //checks the number against the list of existing primes.
         {
-            long root = (long)Math.Ceiling(Math.Sqrt(numberToCheck)); //maybe check to see if it's worth checking if the root is whole
+            ulong root = (ulong)Math.Ceiling(Math.Sqrt(numberToCheck)); //maybe check to see if it's worth checking if the root is whole
             bool isPrime = true;
-            for(long i = 0; primes[(int)i] <= root; i++)
+            for(ulong i = 0; primes[(int)i] <= root; i++)
             {
-                if(numberToCheck%primes[(int)i] == 0) //ignore the cast of a long to an int...
+                if(numberToCheck%primes[(int)i] == 0) //ignore the cast of a ulong to an int...
                 {
                     return false; //if the number is found to not be a prime, return false.
                 }
@@ -45,21 +45,21 @@ namespace primefinder
             Stopwatch tenThousandCounter = new Stopwatch();
             tenThousandCounter.Restart();
             bool quit = false;
-            long numberToCheck = 7;
+            ulong numberToCheck = 7;
             while(!quit)
             {
                 bool isPrime = checkNumber(numberToCheck);
                 if(isPrime)
                 {
                     primes.Add(numberToCheck);
-                    if ((primes.Count + 1) % 1000000 == 0) //every millionth prime it says how long it's been since the last million.
+                    if ((primes.Count + 1) % 1000000 == 0) //every millionth prime it says how ulong it's been since the last million.
                     {
                         Console.WriteLine("The {0} millionth prime was {1}. That took {2} seconds.\nPrimeN|Prime Number|Time(ms)", (primes.Count / 1000000) + 1, numberToCheck, millionCounter.Elapsed.Seconds);
                         millionCounter.Restart();
                     }
                     if ((primes.Count + 1) % 10000 == 0) //every 10,000th prime it says how many ms elapsed.
                     {
-                        Console.WriteLine("{0:000000}|{1:000000000000}|{2}ms", (primes.Count / 10000) + 1, numberToCheck, (long)tenThousandCounter.ElapsedMilliseconds);
+                        Console.WriteLine("{0:000000}|{1:000000000000}|{2}ms", (primes.Count / 10000) + 1, numberToCheck, (ulong)tenThousandCounter.ElapsedMilliseconds);
                         tenThousandCounter.Restart();
                     }
                 }
